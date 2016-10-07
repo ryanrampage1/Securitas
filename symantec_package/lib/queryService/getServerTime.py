a4f9bf645c0de14ba06dab8555f4a5bcf7fdb478
ArrenHgit config --global user.name ArrenHgit config --global user.name ArrenH <huynhall@msu.edu>
2016-10-06 22:33:38 -0400

    getServerTime works

--- symantec_package/lib/queryService/getServerTime.py
+++ symantec_package/lib/queryService/getServerTime.py
@@ -1,18 +1,20 @@
 import logging
 
-def __init__(self,client, requestId):
-    self.client = client
-    self.requestId = requestId
+
+class getServerTime:
+    def __init__(self,client, requestId):
+        self.client = client
+        self.requestId = requestId
 
 
-def __str__(self):
-    res = str(self.client.service.getUserInfo(requestId=self.requestId))
-    return res
+    def __str__(self):
+        res = str(self.client.service.getServerTime(requestId=self.requestId))
+        return res
 
 
-def getFieldContent(self, fieldname):
-    info_list = self.__str__().split('\n')
+    def getFieldContent(self, fieldname):
+        info_list = self.__str__().split('\n')
 
-    for item in info_list:
-        if fieldname in item:
-            return item.split('=')[1][1:]
+        for item in info_list:
+            if fieldname in item:
+                return item.split('=')[1][1:]
